449069582a54d6aa054555e3d77519802f7b8fcb
sangwoo <sangmumab12@gmail.com>
2026-07-30 14:51:46 +0900

    YL-59 Refactor: 회원가입 리뷰 반영 #47 (#50)
    * YL-59 Refactor: 회원가입 리뷰 반영 #47
    
    * YL-59 Refactor: 회원가입 리뷰 반영 롬복 추가 #47

--- src/main/java/com/kb/youngly/dto/auth/SignupRequest.java
+++ src/main/java/com/kb/youngly/dto/auth/SignupRequest.java
@@ -1,57 +1,18 @@
 package com.kb.youngly.dto.auth;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class SignupRequest {
 
     private String loginId;
-
     private String password;
-
     private String name;
-
     private String nickname;
-
     private String email;
 
-    public SignupRequest() {
-    }
-
-    public String getLoginId() {
-        return loginId;
-    }
-
-    public void setLoginId(String loginId) {
-        this.loginId = loginId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/kb/youngly/vo/UserVO.java
+++ src/main/java/com/kb/youngly/vo/UserVO.java
@@ -1,121 +1,27 @@
 package com.kb.youngly.vo;
 
-import com.kb.youngly.enums.UserStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVO {
 
     private String userId;
-
     private String name;
-
     private String loginId;
-
     private String nickname;
-
     private String email;
-
     private String profileImageUrl;
-
     private LocalDateTime createdAt;
-
     private LocalDateTime updatedAt;
-
     private String password;
-
-    private UserStatus userStatus;
-
+    private String userStatus;
     private Long point;
-
-    public UserVO() {
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLoginId() {
-        return loginId;
-    }
-
-    public void setLoginId(String loginId) {
-        this.loginId = loginId;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getProfileImageUrl() {
-        return profileImageUrl;
-    }
-
-    public void setProfileImageUrl(String profileImageUrl) {
-        this.profileImageUrl = profileImageUrl;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserStatus getUserStatus() {
-        return userStatus;
-    }
-
-    public void setUserStatus(UserStatus userStatus) {
-        this.userStatus = userStatus;
-    }
-
-    public Long getPoint() {
-        return point;
-    }
-
-    public void setPoint(Long point) {
-        this.point = point;
-    }
 }
