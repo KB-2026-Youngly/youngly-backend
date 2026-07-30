8100daa3c4eb1c90ed0558615ba18450019e922a
sangsangwoowoo <sangmumab12@gmail.com>
2026-07-30 13:10:48 +0900

    YL-59 Refactor: 회원가입 리뷰 반영 #47

--- src/main/java/com/kb/youngly/config/RootConfig.java
+++ src/main/java/com/kb/youngly/config/RootConfig.java
@@ -17,6 +17,7 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import org.springframework.context.annotation.Import;
 
 /**
  * 루트 애플리케이션 컨텍스트 설정.
@@ -28,6 +29,7 @@ import javax.sql.DataSource;
  * 비밀번호, API Key 등 민감 정보는 application-secret.properties 에 두며
  * 이 파일은 .gitignore 로 제외되어 있다.
  */
+@Import(SecurityConfig.class)
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = {
