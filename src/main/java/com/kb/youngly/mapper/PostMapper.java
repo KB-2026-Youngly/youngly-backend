d5a166a96ca869532457958bf7219e21f8683463
yuna0833 <parkyuna0833@gmail.com>
2026-07-31 16:44:20 +0900

    YL-40 Feat: 그룹 피드 리스트 및 상세 조회 API 구현

--- src/main/java/com/kb/youngly/mapper/PostMapper.java
+++ src/main/java/com/kb/youngly/mapper/PostMapper.java
@@ -4,6 +4,7 @@ import com.kb.youngly.dto.CommentDTO;
 import com.kb.youngly.dto.FeedListResponseDTO;
 import com.kb.youngly.dto.ReactionUserDTO;
 import com.kb.youngly.vo.post.PostVO;
+
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
