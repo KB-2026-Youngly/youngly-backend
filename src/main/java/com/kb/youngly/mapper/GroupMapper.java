4b7326553329a02ec0c04bd102a0df14f254db0c
sangwoo <sangmumab12@gmail.com>
2026-08-04 09:43:11 +0900

    YL-70 Feat: 그룹 생성 API 구현 #79 (#82)
    Co-authored-by: Kanghui <101783655+YO1231@users.noreply.github.com>

---
+++ src/main/java/com/kb/youngly/mapper/GroupMapper.java
@@ -0,0 +1,11 @@
+package com.kb.youngly.mapper;
+
+import com.kb.youngly.vo.group.GroupVO;
+
+public interface GroupMapper {
+
+    /**
+     * 그룹 생성
+     */
+    void insertGroup(GroupVO group);
+}
