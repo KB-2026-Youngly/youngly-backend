4ee0a50d71a5fc2c03b2c7c5b02298c08ca98b39
yuna0833 <144637705+yuna0833@users.noreply.github.com>
2026-08-04 09:41:12 +0900

    YL-41 Feat: 그룹 생성 API 구현 #27 (#77)
    * YL-36 Feat: 이미지 업로드 API #24
    
    * YL-40 Feat: 그룹 피드 리스트 및 상세 조회 API 구현
    
    * YL-41 Feat: 인증 승인·반려 API, #27

--- src/main/java/com/kb/youngly/dto/posts/CommentDTO.java
+++ src/main/java/com/kb/youngly/dto/posts/CommentDTO.java
@@ -1,14 +1,19 @@
-// ReactionUserDTO.java (좋아요/싫어요 누른 유저 정보)
-package com.kb.youngly.dto;
+// CommentDTO.java (댓글 DTO)
+
+package com.kb.youngly.dto.posts;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReactionUserDTO {
+public class CommentDTO {
+    private Long postCommentId;
+    private String content;
+    private LocalDateTime createdAt;
     private String userId;
     private String nickname;
     private String profileImageUrl;
--- src/main/java/com/kb/youngly/dto/posts/FeedDetailResponseDTO.java
+++ src/main/java/com/kb/youngly/dto/posts/FeedDetailResponseDTO.java
@@ -2,7 +2,7 @@
 // 피드 상세 화면을 위한 세트
 // : CommentDTO(댓글 목록), ReactionUserDTO(좋아요 누른 유저 목록), ReactionUserDTO(싫어요 누른 유저 목록)를 각각 리스트(List<>) 형태로 한 번에 묶어서 반환
 
-package com.kb.youngly.dto;
+package com.kb.youngly.dto.posts;
 
         import lombok.AllArgsConstructor;
         import lombok.Data;
